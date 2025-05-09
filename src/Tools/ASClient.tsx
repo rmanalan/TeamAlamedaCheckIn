ec1bb410ef4711c94c68806191949e74e8bb762f
Rich Manalang <rich@arcade.software>
2025-05-08 17:16:38 -0700

    Fix

--- src/Tools/ASClient.tsx
+++ src/Tools/ASClient.tsx
@@ -1,11 +1,11 @@
 // apps script client
 export class ASClient {
     // HIDE ME EVENTUALLY
-    #sheet = "https://script.google.com/macros/s/AKfycbzushZ9-lh3mxA5kZ34SWMa1ffGwmF9FjnmxMXsSpi6EyF2cXU_2cUiRLjx36W1VvIEFw/exec"
+    asScriptUrl = "https://script.google.com/macros/s/AKfycbzushZ9-lh3mxA5kZ34SWMa1ffGwmF9FjnmxMXsSpi6EyF2cXU_2cUiRLjx36W1VvIEFw/exec"
 
-    fetch(route:string, data:any) {
+    fetch(route:string, data:unknown) {
         return new Promise<string>((res, rej) => {
-            fetch(this.#sheet, {method:"post", body:JSON.stringify({route:route,data:data})}).
+            fetch(this.asScriptUrl, {method:"post", body:JSON.stringify({route:route,data:data})}).
             then(r => r.text().then(t => {
                 console.log(t)
                 if (t.startsWith("OK")) {
